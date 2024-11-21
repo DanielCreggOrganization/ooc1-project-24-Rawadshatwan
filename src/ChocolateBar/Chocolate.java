f641afbeeaee61377107f833105a43b3364690af
rawadshatwan <145453561+Rawadshatwan@users.noreply.github.com>
2024-11-21 16:00:53 +0000

    include Price and Quantity

--- src/ChocolateBar/Chocolate.java
+++ src/ChocolateBar/Chocolate.java
@@ -1,8 +1,12 @@
 package ChocolateBar;
 
-public class ChocolateBar {
+public class Chocolate {
         protected String Brand;
-        protected int Flavour;
+        protected String Flavour;
+        protected String Price;
+        protected int Quntity;
+        
+                
     
         public void Bar(){
             System.err.println("This is a" + Brand + "with a" + Flavour + "taste");
