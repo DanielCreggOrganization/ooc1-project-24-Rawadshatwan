7386b6e26f23b0dbdb3baab7ca0d759cb4d6a60f
rawadshatwan <145453561+Rawadshatwan@users.noreply.github.com>
2024-11-21 16:50:26 +0000

    sss

--- src/ChocolateBar/Main.java
+++ src/ChocolateBar/Main.java
@@ -1,11 +1,34 @@
 package ChocolateBar;
 
+import java.util.Scanner;
+
+
 public class Main {
 
 public static void main(String[] args) {
+
    
-    Chocolate chocolateObject1 = new Chocolate(1384325, "Kinder", 175, 8);
-    System.out.println(chocolateObject1.getBrand());
+   while (true) {
+    System.out.println("");
+    System.out.println("###############################################");
+    System.out.println("            Chocolate Bar Plaza                ");
+    System.out.println("###############################################");
+    System.out.println("(1) Add a Chocolate Bar");
+    System.out.println("(2) Delete a Chocolate Bar");
+    System.out.println("(3) Show total Number of Students.");
+    System.out.println("(4) Search for a Student by 10");
+    System.out.println("(5) QUIT.");
+    System.out.println("(pick an option");
+
+    userSelection = userInput.nextInt();
+
+    if (userSelection == 5) {
+        System.out.println("Chocolate Bar Plaza is melting");
+        userInput.close();
+        break
     }
+}
+
+}
 
 }
