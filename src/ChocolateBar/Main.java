dcba11748c835795eaa3eba7cc537c3b5b4f823c
rawadshatwan <145453561+Rawadshatwan@users.noreply.github.com>
2024-11-21 16:32:37 +0000

    brand

--- src/ChocolateBar/Main.java
+++ src/ChocolateBar/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
 public static void main(String[] args) {
    
-    Chocolate ChocolateObject1 = new Chocolate(1384325, "Kinder", 1.75, 8)
+    Chocolate ChocolateObject1 = new Chocolate(1384325, "Kinder", 175, 8);
+    System.out.println(ChocolateObject1.getBrand());
+    }
+
 }
