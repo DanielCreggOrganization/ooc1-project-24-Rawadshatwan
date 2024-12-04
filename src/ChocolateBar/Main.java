bb6fc7fde46a1671a1c263637258bd2d93aa828a
rawadshatwan <145453561+Rawadshatwan@users.noreply.github.com>
2024-12-04 15:25:32 +0000

    i thought i had some thing but i didn't =(

--- src/ChocolateBar/Main.java
+++ src/ChocolateBar/Main.java
@@ -18,18 +18,43 @@ public static void main(String[] args) {
     System.out.println("###############################################");
     System.out.println("(1) Add a Chocolate Bar");
     System.out.println("(2) Delete a Chocolate Bar");
-    System.out.println("(3) Show total Number of Students.");
-    System.out.println("(4) Search for a Student by 10");
+    System.out.println("(3) Show total Number of Chocolate Bar.");
+    System.out.println("(4) Search for a Chocolate Bar by 10");
     System.out.println("(5) QUIT.");
-    System.out.println("(pick an option");
+    System.out.println("(pick an option)");
 
     userSelection = userInput.nextInt();
+
+    if (userSelection == 1) {
+        System.out.println("Choose a Chocolate Bar");
+        userInput.close();
+        continue;
+    }
+
+    if (userSelection == 2) {
+        System.out.println("");
+        userInput.close();
+        break;
+    }
+
+    if (userSelection == 3) {
+        System.out.println("");
+        userInput.close();
+        break;
+    }
+
+    if (userSelection == 4) {
+        System.out.println("");
+        userInput.close();
+        break;
+    }
 
     if (userSelection == 5) {
         System.out.println("Chocolate Bar Plaza is melting");
         userInput.close();
         break;
     }
+
 }
 
 }
